2ad81e6a7e3ad383a2028fa357166399d16a0d84
malogalbo <mlogalbo@ufl.edu>
2024-02-01 12:29:05 -0500

    got ugly simple logistic

--- test app/app.R
+++ test app/app.R
@@ -2,38 +2,47 @@ library(shiny)
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-  
+
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("Logistic Curve"),
+
+  #Logit Form ----
+  mainPanel(withMathJax("$$\\text{Logit Form: }\\frac{\\pi}{1-\\pi}=\\beta_0+\\beta_1 x \\hspace{1cm} \\text{Probability Form: }\\pi = \\frac{e^{\\beta_0+\\beta_1 x}}{1+e^{\\beta_0+\\beta_1 x}}$$")),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
+
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+
       # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
+      sliderInput(inputId = "beta0",
+                  label = withMathJax("$$\\text{Intercept}\\hspace{.25cm}\\beta_0$$"),
+                  min = -5,
+                  max = 5,
+                  value = 0),
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "beta1",
+                  label = withMathJax("$$\\text{Slope}\\hspace{.25cm}\\beta_1$$"),
+                  min = -5,
+                  max = 5,
+                  value = 0),
+
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
-      
+
       # Output: Histogram ----
       plotOutput(outputId = "distPlot")
-      
+
     )
   )
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  
+
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
   # This expression that generates a histogram is wrapped in a call
@@ -44,15 +53,26 @@ server <- function(input, output) {
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    x_values <- seq(-5, 5, length.out = 100)
     
-    hist(x, breaks = bins, col = "#007bc2", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+    # Logistic function formula
+    logistic_function <- function(x, a, c) {
+      y <- 1 / (1 + exp(-(a * (x) + c)))
+      return(y)
+    }
     
-  })
-  
-}
+    y_values <- logistic_function(x_values, input$beta1, input$beta0)
 
+    plot_data <- data.frame(x = x_values, y = y_values)
+    ggplot(plot_data, aes(x, y)) +
+      geom_line() +
+      labs(title = "Logistic Function",
+           x = "x",
+           y = withMathJax("$$\\pi$$"))
+
+  })
+
+}
 shinyApp(ui = ui, server = server)
+
+# 
